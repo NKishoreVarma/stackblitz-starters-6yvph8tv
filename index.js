23047f0851158a0ce39a8a3dd1602f9357ab48d6
Kishore Varma <kishorevarma2205@gmail.com>
2025-02-21 14:57:58 +0530

    .

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose=require("mongoose")
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+require('dotenv').config();
+
+
+mongoose.connect(process.env.mongo_url)
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log('Error connecting to database:', err));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
@@ -13,3 +20,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
